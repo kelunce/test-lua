1415417ef4ebb007650071fb0512076f0d2bf714
李程满 <lichengman@163.com>
2019-04-07 23:35:33 +0800

    #feature add test

--- 5-call_module.lua
+++ 5-call_module.lua
@@ -1,6 +1,14 @@
 #!/bin/env lua
 
 --require 有它自己的文件路径加载策略，它会尝试从 Lua 文件或 C 程序库中加载模块。
+--通过监视loaded发现新加载的模块
+setmetatable(package.loaded, {__newindex = function(t, k, v)
+    print("***load new mod:"..tostring(k))
+    rawset(t, k, v)
+end})
+
+require("4-module")
+--package.loaded["4-module"] = nil
 require("4-module")
 
 print(module.constant)
@@ -17,4 +25,10 @@ local function f_bin2hex(s)
 end
 
 local f = require("md5.core")
-print(f_bin2hex(f.sum("xxxx"))) --EA416ED0759D46A8DE58F63A59077499
+print("md5sum(xxxx)=",f_bin2hex(f.sum("xxxx"))) --EA416ED0759D46A8DE58F63A59077499
+
+
+--print("requre md5 mode ret:", f)
+--for k,v in  pairs(f) do
+--    print(k, v)
+--end
